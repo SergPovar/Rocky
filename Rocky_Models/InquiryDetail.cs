c2256b3d15add8a0cb9e41dc86ceb9055955cb3c
Serg Terekhov <sergterekhov4902@gmail.com>
2024-01-29 23:09:47 +0300

    First

--- Rocky_Models/InquiryDetail.cs
+++ Rocky_Models/InquiryDetail.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,15 @@ namespace Rocky_Models
 {
     public class InquiryDetail
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string InquiryHeaderId { get; set; }
+        [ForeignKey(nameof(InquiryHeaderId))]
+        public InquiryHeader InquiryHeader { get; set; }
+        [Required]
+        public string ProductId { get; set; }
+        [ForeignKey(nameof(ProductId))]
+        public Product Product { get; set; }
     }
 }
